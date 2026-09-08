a30be3bfe850f8f3c5ba3ce3c7cf433331fc581d
Ankit <ankitatserenity@gmail.com>
2026-09-09 01:37:20 +0530

    used cmake to build and run a cpp with self made lib

---
+++ build/CMakeFiles/mymath.dir/mymath.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/mymath.dir/mymath.cpp.o: \
+ /home/ankit/Code_purpose/ns3-crypto-integration/cmakeprac/mymath.cpp \
+ /usr/include/stdc-predef.h \
+ /home/ankit/Code_purpose/ns3-crypto-integration/cmakeprac/mymath.h
---
+++ prac.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "mymath.h"
+using namespace std;
+
+int main(){
+    cout << "hi there" << endl;
+    cout << "2 + 3 = " << add(2,3) << endl;
+    return 0;
+}
